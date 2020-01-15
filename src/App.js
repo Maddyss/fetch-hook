9934596833b2e3210e0646a1a2d6b69de6859623
kleg <contact@winnzos.fr>
2020-01-15 20:00:55 +0100

    Gestion option fetch + conf publish

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import useCustomFetch from './useFetch';
+import useFetchJson from './useFetch';
 
 function App() {
-  const [state] = useCustomFetch('https://swapi.co/api/people', {});
+  const [{ isLoading, error, data }] = useFetchJson(
+    'https://swapi.co/api/people',
+    {
+      method: 'GET'
+    }
+  );
 
-  if (state.isLoading) {
+  if (isLoading) {
     return <div className='App'>En chargement ...</div>;
   }
   return (
@@ -15,9 +20,9 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          {state.data &&
-            state.data.results &&
-            state.data.results.map(res => {
+          {data &&
+            data.results &&
+            data.results.map(res => {
               return <p>{res.name}</p>;
             })}
         </p>
--- src/useFetch.js
+++ src/useFetch.js
@@ -2,19 +2,20 @@ import { useEffect, useReducer } from 'react';
 
 import reducer, { actions, initialState } from './reducer';
 
-export default function useCustomFetch(url) {
+export default function useFetchJson(url, options) {
   const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(() => {
     (async () => {
       dispatch({ type: actions.REQUEST_START });
       try {
-        const response = await fetch(url, {});
+        const response = await fetch(url, { ...options });
         const data = await response.json();
         dispatch({ type: actions.REQUEST_SUCCESS, payload: data });
       } catch (e) {
         dispatch({ type: actions.REQUEST_FAIL, payload: e });
       }
     })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [url]);
 
   return [state];
